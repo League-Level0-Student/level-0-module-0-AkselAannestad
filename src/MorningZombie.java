77d6351bc8ecc5a6e9474dabe76cb97f82228f46
Jsmith2021 <league@WTS-iMac-11.attlocal.net>
2019-04-16 18:26:09 -0700

    first day of level 0
    I was nervous, but it was all ok
    I finished up something from last session (writing my name), and then
    made a thing about my morning routine. It was all pretty familiar.
    Level 0= 0 problem (so far)

---
+++ src/MorningZombie.java
@@ -0,0 +1,22 @@
+import javax.swing.JOptionPane;
+
+public class MorningZombie {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null, "I suppose I'll walk you through my morning routine. I suggest you leave before I subject you to listening to me waste your time by telling you about how I waste my time");
+	JOptionPane.showMessageDialog(null, "Step 1: I hear my alarm go off.");
+	JOptionPane.showMessageDialog(null, "Step 2: I sit there until I can't take the beeping anymore.");
+	JOptionPane.showMessageDialog(null, "Step 3: I hit snooze.");
+	JOptionPane.showMessageDialog(null, "Step 4: I repeat step three for an undetermined amount of time");
+	JOptionPane.showMessageDialog(null, "Step 4: I force myself to turn off the alarm and get up.");
+	JOptionPane.showMessageDialog(null, "Step 5: I still am half alseep so I wouldn't notice anything such as me putting step four twice (but I bet you didn't either so don't judge).");
+	JOptionPane.showMessageDialog(null, "Step 6: I go to the bathroom and de-ordorize myself (for the public welfare).");
+	JOptionPane.showMessageDialog(null, "Step 7: I go back to my room and put on some clothes. Note that this is arguably the most important step");
+	JOptionPane.showMessageDialog(null, "Step 8: I slog downstairs, grab my phone and start playing some music while I eat some food. Toast, cereal, yogurt, whatever.");
+	JOptionPane.showMessageDialog(null, "Step 9: I probably eat for a while, seeing as I'm distracted by my phone. But hey, why be bored while I breakfast?");
+	JOptionPane.showMessageDialog(null, "Step 10:I go back upstairs and brush my teeth. I also grab my backpack and socks from my room.");
+	JOptionPane.showMessageDialog(null, "Step 11:While I pack stuff in my bag, I also busy myself by being needlessly stressed about being late even though I still have plent of time to walk.");
+	JOptionPane.showMessageDialog(null, "Step 12:I am ready");
+	JOptionPane.showMessageDialog(null, "Step 13:I realize that all my needless stressing really was needless as my neighbor who I walk to school with won't be ready for another 5 minutes");
+	JOptionPane.showMessageDialog(null, "Alternate Method:On the weekend, I change my routine by doing nothing on any sort of shcedule. I kind of avoid doing anything. ");
+}
+}
--- src/strings_and_dialogs/Robot_Graffiti.java
+++ src/strings_and_dialogs/Robot_Graffiti.java
@@ -1,12 +1,20 @@
 package strings_and_dialogs;
 
+import java.awt.event.TextEvent;
+
+import javax.swing.plaf.FontUIResource;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class Robot_Graffiti {
 public static void main(String[] args) {
 	Robot g=new Robot();
+	g.setSpeed(100);
+	g.turn(-90);
+	g.move(300);
+	g.turn(90);
 	g.penDown();
-	g.setSpeed(50);
+	g.setSpeed(100);
 	g.turn(30);
 	g.move(240);
 	g.turn(180);
@@ -26,8 +34,65 @@ public static void main(String[] args) {
 	
 	g.move(200);
 	g.turn(180);
+	g.move(65);
+	g.turn(-145);
+	g.move(65);
+	g.turn(180);
 	g.move(60);
-	g.turn()
+	g.turn(-70);
+	g.move(170);
+	
+	g.turn(180);
+	g.move(170);
+	g.turn(-145);
+	g.penUp();
+	g.move(135);
+	g.turn(-90);
+	g.move(200);
+	g.penDown();
+	g.setSpeed(100);
+	g.hide();
+	for (int i = 0; i < 180; i++) {
+		g.move(1);
+	g.turn(-1);
+	}
+	for (int i = 0; i <180; i++) {
+		g.move(1);
+		g.turn(1);
+	}
+	g.penUp();
+	g.show();
+	g.move(200);
+	g.turn(90);
+	
+	g.penDown();
+	g.move(200);
+	g.turn(-90);
+	g.move(50);
+	g.turn(180);
+	g.move(50);
+	g.turn(90);
+	g.move(100);
+	g.turn(90);
+	g.move(50);
+	g.turn(180);
+	g.move(50);
+	g.turn(90);
+	g.move(100);
+	g.turn(90);
+	g.move(50);
+	g.turn(180);
+	g.move(50);
+	g.turn(180);
+	g.penUp();
+	g.move(250);
+	g.turn(90);
+	g.penDown();
+	g.move(200);
+	g.turn(-90);
+	g.move(50);
+	g.penUp();
+	g.move(100);
 	
 	
 }
